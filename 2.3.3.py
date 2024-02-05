ab6e5bdeedc1d4cf434859a2e25c2fc9da246382
arsenii <arsent.ru@gmail.com>
2024-02-05 20:31:16 +0300

    completed step 2.3.3 altered link in 2.3.2

--- 2.3.3.py
+++ 2.3.3.py
@@ -1,7 +1,7 @@
 import socketio
 import eventlet
 
-# Степ https://stepik.org/lesson/1210368/step/2?unit=1223619
+# Степ https://stepik.org/lesson/1210368/step/3?unit=1223619
 
 # Создаем экземпляр сервера Socket.IO
 sio = socketio.Server(cors_allowed_origins='*')
@@ -9,15 +9,21 @@ sio = socketio.Server(cors_allowed_origins='*')
 # Создаем WSGI приложение
 app = socketio.WSGIApp(sio)
 
+users = []
 
 # Обработчик события соединения
 @sio.on('connect')
 def connect(sid, environ):
-    sio.emit("message", to=sid, data={"message": "Welcome to the server"})
+    users.append(sid)
+    sio.emit("message", to=sid, data={
+        "users": users,
+        "online": len(users)
+    })
 
 
 @sio.event
 def disconnect(sid):
+    users.remove(sid)
     print(f"sid {sid} discconected :(")
 
 
